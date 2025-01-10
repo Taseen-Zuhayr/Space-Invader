629a9632cdbc7dc238ae5eb00493a1f4129da6a9
Taseen-Zuhayr <taseen.zuhayr@gmail.com>
2025-01-10 22:53:22 +0600

    Beta Version

--- Main.py
+++ Main.py
@@ -80,4 +80,98 @@ textX = 10
 textY = 10
 
 
+over_font = pygame.font.Font('freesansbold.ttf', 64)
+
+
+def show_score(x, y):
+
+    score = font.render("Score : " + str(score_value), True, (255,255,255))
+    screen.blit(score, (x, y))
+
+
+def game_over_text():
+
+    over_text = over_font.render("GAME OVER", True, (255,255,255))
+    screen.blit(over_text, (200,250))
+
+
+def player(x, y):
+
+    screen.blit(player_image, (x, y))
+
+
+def enemy(x, y, i):
+
+    screen.blit(enemy_image[i], (x, y))
+
+
+def fire_bullet(x, y):
+
+    global bullet_state
+
+    bullet_state = "fire"
+    screen.blit(bullet_image, (x + 16, y + 10))
+
+
+def isCollision(enemyX, enemyY, bulletX, bulletY):
+
+    distance = math.sqrt((enemyX - bulletX) ** 2 + (enemyY - bulletY) ** 2)
+    return distance < collision_distance
+
+
+running = True
+
+while running:
+    
+    screen.fill((0,0,0))
+    screen.blit(background, (0,0))
+
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            running = False
+        
+        if event.type == pygame.KEYDOWN:
+            
+            if event.key == pygame.K_LEFT:
+                playerX_change = -5
+
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 5
+
+            if event.key == pygame.K_SPACE and bullet_state == "ready":
+
+                bulletX = playerX
+                fire_bullet(bulletX, bulletY)
+
+        
+        if event.type == pygame.KEYUP and event.key in [pygame.K_LEFT, pygame.K_RIGHT]:
+
+            playerX_change = 0
+
+
+    playerX += playerX_change
+    playerX = max(0, min(playerX, screen_width - 64))
+
+
+    for i in range(num_of_enemies):
+
+        if enemyY[i] > 340:
+
+            for j in range(num_of_enemies):
+
+                enemyY[j] = 2000
+            
+            game_over_text()
+            break
+
+
+        enemyX[i] += enemyX_change[i]
+
+        if enemyX[i] <= 0 or enemyX[i] >= screen_width - 64:
+
+            enemyX_change[i]
+
+
 
